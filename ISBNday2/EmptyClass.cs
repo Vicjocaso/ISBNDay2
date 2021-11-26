d1b9ba87ef26b485de8cb8593fee507e9e1f2de9
victor Jose Calderon <victorjosecaderon@victors-MacBook-Air-2.local>
2021-11-26 11:47:44 -0400

    day2

---
+++ ISBNday2/EmptyClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace ISBNday2
+{
+    public class EmptyClass
+    {
+        public static bool VerifyISBN(int[] number)
+        {
+            int multiplicacion = 0;
+            int suma = 0;
+
+            if (number.Length == 10)
+            {
+                int position = 10;
+
+                for (int i = 0; i < number.Length; i++)
+                {
+                    multiplicacion = number[i] * position;
+                    suma = suma + multiplicacion;
+                    position--;
+                    multiplicacion = 0;
+                }
+                if(suma % 11 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if(number.Length == 13)
+            {
+                for (int i = 0; i < number.Length; i++)
+                {
+                    if (number[i] % 2 == 0)
+                    {
+                        multiplicacion = number[i] * 3;
+                    }
+                    else
+                    {
+                        multiplicacion = number[i] * 1;
+                    }
+                    suma += multiplicacion;
+                    multiplicacion = 0;
+                }
+                if(suma % 10 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ISBNday2/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ISBNday2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] number = new int[] { 9, 7, 8, 0, 7, 1, 6, 7, 0, 3, 4, 4, 0 };
+
+            bool result = EmptyClass.VerifyISBN(number);
+
+            Assert.AreEqual(result, true);
+        }
+    }
+}
